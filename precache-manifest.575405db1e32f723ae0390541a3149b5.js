fd12befa57a0e41a6f13e6f11528884cd5a05503
Pat Doyle <patrick.doyle126@gmail.com>
2020-04-19 12:12:17 -0500

    Updates

---
+++ precache-manifest.575405db1e32f723ae0390541a3149b5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3f5654541b5903f8dac0c41bc57f75e",
+    "url": "/hegic-frontend/index.html"
+  },
+  {
+    "revision": "e6445da804d0cb548773",
+    "url": "/hegic-frontend/static/css/main.d35f1eb5.chunk.css"
+  },
+  {
+    "revision": "50ddb2c6357cc7aac057",
+    "url": "/hegic-frontend/static/js/2.f594538f.chunk.js"
+  },
+  {
+    "revision": "49423731f4b42bb1acb1fefa8741c67d",
+    "url": "/hegic-frontend/static/js/2.f594538f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6445da804d0cb548773",
+    "url": "/hegic-frontend/static/js/main.a1558a2b.chunk.js"
+  },
+  {
+    "revision": "058ea43ecb522d83abb4",
+    "url": "/hegic-frontend/static/js/runtime-main.b744cb92.js"
+  },
+  {
+    "revision": "14c35a626d4aa99499664ab05c04ac52",
+    "url": "/hegic-frontend/static/media/download-metamask.14c35a62.png"
+  },
+  {
+    "revision": "8fcbc12dbb44c178ed09323b1ec398e7",
+    "url": "/hegic-frontend/static/media/switch-network.8fcbc12d.jpg"
+  }
+]);
